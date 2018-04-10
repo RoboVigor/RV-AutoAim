c4b8959cef4825704946f484255434dfa6828ae9
tccoin <1774241372@qq.com>
2018-04-11 01:22:24 +0800

    imshow

--- autoaim/autoaim.py
+++ autoaim/autoaim.py
@@ -3,5 +3,11 @@ import cv2
 import os
 
 mat = cv2.imread('../data/hotel.jpg')
-cv2.imshow('Original Image', mat)
+b, g, r = cv2.split(mat)
+cv2.imshow('Original', mat)
+ret, thresh = cv2.threshold(b, 20, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+canny = cv2.Canny(b, 50, 150)
+cv2.imshow('threshold', thresh)
+cv2.imshow('canny', canny)
 cv2.waitKey(0)
+cv2.destroyAllWindows()
