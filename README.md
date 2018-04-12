dc9ef83b1b98401e4662b542da81232cd200cc58
tccoin <1774241372@qq.com>
2018-04-12 23:46:32 +0800

    new API!

--- README.md
+++ README.md
@@ -3,6 +3,24 @@
 来，携手共进，开发属于我们的自瞄外挂！
 
 [开发日志](https://note.krrr.party/#/article/article-792559)
+## Usage
+
+```python
+autoaim = ImgToolbox('path/to/img')
+autoaim.mat = autoaim.r # use the green channel (default)
+autoaim.drawMat = autoaim.src # use the source image to draw (default)
+autoaim.pairLamps(True) # use the method while drawing
+autoaim.show('o'*8+'h')
+```
+
+## ImgToolbox
+
+```python
+threshold(draw=False)
+findContours(draw=False, thresh=None)
+findLamps(draw=False, contours=None, rects=None)
+pairLamps(draw=False, lamps=None)
+```
 
 ## 目录
 
--- autoaim/autoaim.py
+++ autoaim/autoaim.py
@@ -3,58 +3,51 @@ import cv2
 import numpy
 import math
 
+
 class Lamp():
-    def __init__(self, contour, rect, greyscale):
+    def __init__(self, contour, rect, greyscale, area):
         self.contour = contour
         self.greyscale = greyscale
         self.x = rect[0]
         self.y = rect[1]
         self.w = rect[2]
         self.h = rect[3]
-        self.a = self.w*self.h
+        self.a = area
         self.paired = False
     def __lt__(self, other):
         return self.x < other.x
     def __eq__(self, other):
         return self.x==other.x and self.y==other.y and self.w==other.w and self.h==other.h
 
-class Autoaim():
+
+class ImgToolbox():
 
     def __init__(self, img):
         if isinstance(img, str):
-            self.mat = cv2.imread(img)
+            self.src = cv2.imread(img)
         else:
-            self.mat = img
-        if len(self.mat.shape) == 3:
-            self.b, self.g, self.r, = cv2.split(self.mat)
+            self.src = img
+        if len(self.src.shape) == 3:
+            self.b, self.g, self.r, = cv2.split(self.src)
+        self.mat = self.g
+        self.drawMat = self.src.copy()
 
-    def show(self):
-        cv2.imshow('original image', self.mat)
+    def show(self, winname):
+        cv2.imshow(winname, self.drawMat)
 
-    def threshold(self, mat=None, winname=None):
-        if mat is None:
-            mat = self.mat
+    def threshold(self, draw=False):
+        mat = self.drawMat
+        if not draw:
+            mat = self.mat.copy()
         ret, thresh = cv2.threshold(mat, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-        print('threshold: ', ret)
-        if not winname is None:
-            cv2.imshow(winname, thresh)
         return thresh
 
-    def canny(self, mat=None, winname=None):
-        if mat is None:
-            mat = self.mat
-        canny = cv2.Canny(mat, 50, 150)
-        if not winname is None:
-            cv2.imshow(winname, canny)
-        return canny
-
-    def findPossibleContours(self, mat=None, winname=None):
-        if mat is None:
-            mat = self.g
-        # binarize
-        src = self.threshold(mat)
+    def findContours(self, draw=False, thresh=None):
+        # threshold
+        if thresh is None:
+            thresh = self.threshold()
         # find contours
-        all_contours = cv2.findContours(src, cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)[1] #get the contour
+        all_contours = cv2.findContours(thresh, cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)[1]
         # find area between 32 and 4096
         possible_contours = []
         possible_rects = []
@@ -62,25 +55,23 @@ class Autoaim():
             contour = all_contours[i]
             x,y,w,h = cv2.boundingRect(contour)
             area = w*h
-            if area >32 and area < 4096:
+            if area>32 and area<4096:
                 possible_contours.append(contour)
                 possible_rects.append((x,y,w,h))
-        # draw the contours
-        if not winname is None:
-            _mat = self.mat.copy()
-            #cv2.drawContours(_mat,all_contours,-1,(0,255,0),1)
-            cv2.drawContours(_mat,possible_contours,-1,(0,0,255),3)
-            cv2.imshow(winname, _mat)
-            print('find '+str(len(possible_contours))+' possible contours.')
+        # draw
+        if draw:
+            drawMat = self.drawMat
+            cv2.drawContours(drawMat,possible_contours,-1,(0,0,255),3)
         return possible_contours, possible_rects
 
-    def findLamp(self, mat=None, winname=None):
-        if mat is None:
-            mat = self.g
+    def findLamps(self, draw=False, contours=None, rects=None):
+        mat = self.mat
         # find contours
-        contours, rects = self.findPossibleContours(mat)
-        # calculate greyscale value for every contour
+        if contours is None or rects is None:
+            contours, rects = self.findContours()
+        # calculate greyscale and area
         greyscales = []
+        areas = []
         for i in range(0, len(contours)):
             contour = contours[i]
             cimg = numpy.zeros_like(mat)
@@ -89,14 +80,34 @@ class Autoaim():
             pts = mat[pts[0], pts[1]]
             greyscale = sum(pts)/len(pts)
             greyscales.append(greyscale)
+            areas.append(len(pts))
         # determine the lamp
         lamps = []
         for i in range(0, len(rects)):
             rect = rects[i]
             if greyscales[i] > 205 and rect[3]/rect[2] > 2:
-                lamp = Lamp(contours[i], rects[i], greyscales[i])
+                lamp = Lamp(contours[i], rects[i], greyscales[i], areas[i])
                 lamps.append(lamp)
         lamps.sort()
+        # draw
+        if draw:
+            drawMat = self.drawMat
+            for i in range(0, len(lamps)):
+                lamp = lamps[i]
+                cv2.rectangle(drawMat,(lamp.x,lamp.y),
+                        (lamp.x+lamp.w,lamp.y+lamp.h),
+                        (200,200,200)
+                    )
+                cv2.putText(drawMat, str(math.floor(lamp.greyscale)),
+                        (lamp.x,math.floor(lamp.y+lamp.h+10)),
+                        cv2.FONT_HERSHEY_COMPLEX,0.4,(200,200,200),1
+                        )
+        return lamps
+
+    def pairLamps(self, draw=False, lamps=None):
+        # find lamps
+        if lamps is None:
+            lamps = self.findLamps(draw)
         # pair the lamp
         pair_lamps = []
         _lamps = lamps.copy()
@@ -108,8 +119,9 @@ class Autoaim():
                 for j in range(i+1, len(_lamps)):
                     _lamp_right = _lamps[j]
                     if not _lamp_right.paired:
-                        _pair_score = abs(lamp_left.y-_lamp_right.y) + abs(lamp_left.a - _lamp_right.a)/lamp_left.a*7.5
-                        print(abs(lamp_left.a - _lamp_right.a)/lamp_left.a)
+                        # score
+                        _pair_score = (abs(lamp_left.y-_lamp_right.y)
+                            + abs(lamp_left.a - _lamp_right.a)/lamp_left.a*5)
                         if _pair_score<pair_score:
                             pair_score = _pair_score
                             lamp_right = _lamp_right
@@ -117,28 +129,29 @@ class Autoaim():
                     pair_lamps.append([lamp_left,lamp_right])
                     lamp_left.paired = True
                     lamp_right.paired = True
-        # show the result
-        if not winname is None:
-            _mat = self.mat.copy()
-            #cv2.drawContours(_mat,contours,-1,(0,0,255),2)
-            for i in range(0, len(lamps)):
-                lamp = lamps[i]
-                cv2.putText(_mat, str(math.floor(lamp.a)),
-                        (lamp.x,math.floor(lamp.y+lamp.h+10)),
-                        cv2.FONT_HERSHEY_COMPLEX,0.4,(200,200,200),1
-                        )
+        # draw
+        if draw:
+            drawMat = self.drawMat
             for i in range(0, len(pair_lamps)):
                 pair_lamp = pair_lamps[i]
-                cv2.rectangle(_mat,(pair_lamp[0].x,pair_lamp[0].y),(pair_lamp[1].x+pair_lamp[1].w,pair_lamp[1].y+pair_lamp[1].h),(255,255,0))
-            cv2.imshow(winname, _mat)
-
+                cv2.rectangle(drawMat,(pair_lamp[0].x,pair_lamp[0].y),
+                        (pair_lamp[1].x+pair_lamp[1].w,pair_lamp[1].y+pair_lamp[1].h),
+                        (255,255,0)
+                    )
+        return pair_lamps
 
 
 if __name__ == '__main__':
-    for i in range(1,5):
-        autoaim = Autoaim('../data/miao'+str(i)+'.jpg')
-        autoaim.findLamp(autoaim.g, 'g'+str(i))
-    autoaim = Autoaim('../data/hotel.jpg')
-    autoaim.findLamp(autoaim.g, 'g'+str(i))
+    '''
+    for i in range(0,6):
+        autoaim = ImgToolbox('../data/miao'+str(i)+'.jpg')
+        autoaim.pairLamps(True)
+        autoaim.show('miao'+str(i)+'.jpg')
+    '''
+    autoaim = ImgToolbox('../data/miao2.jpg')
+    autoaim.pairLamps(True)
+    #autoaim.show('miao '*3)
+    autoaim.show('o'*8+'h')
+
     cv2.waitKey(0)
     cv2.destroyAllWindows()
