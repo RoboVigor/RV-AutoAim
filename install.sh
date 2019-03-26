40a6118ad3c07abe6dbfe6208d5e63ec8c5d33b9
tccoin <1774241372@qq.com>
2019-03-26 18:04:02 +0800

    update for new test7 @2.5.1

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 cd data
 rm test?/ -rf
-wget https://github.com/RoboVigor/RV-AutoAim-Data/archive/2.4.1.zip
-unzip 2.4.1.zip
-mv RV-AutoAim-Data-2.4.1/data/* .
-rm RV-AutoAim-Data-2.4.1 -rf
-rm 2.4.1.zip
+wget https://github.com/RoboVigor/RV-AutoAim-Data/archive/2.4.2.zip
+unzip 2.4.2.zip
+mv RV-AutoAim-Data-2.4.2/data/* .
+rm RV-AutoAim-Data-2.4.2 -rf
+rm 2.4.2.zip
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('LICENSE') as f:
 
 setup(
     name='AutoAim',
-    version='2.5.0',
+    version='2.5.1',
     description='A project for detecting armors.',
     author='FuXing PS',
     author_email='robovigor@gmail.com',
