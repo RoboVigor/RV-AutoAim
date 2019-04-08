e2e91c65b54cafba70cad55bfafcefc7d2395836
tccoin <1774241372@qq.com>
2019-04-08 23:45:28 +0800

    update tests

--- tests/test_autoaim.py
+++ tests/test_autoaim.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 import unittest
 import os
+import autoaim
+from autoaim import *
 import serial
 import serial.tools.list_ports
-import autoaim
 
 data1 = [87, 16, 0, 6, 0, 88]
 port_list = list(serial.tools.list_ports.comports())
@@ -14,10 +15,11 @@ isTravis = bool(os.environ.get('TRAVIS_PYTHON_VERSION'))
 class SerialTestSuite(unittest.TestCase):
 
     def test_crc_calculation(self):
-        crc = autoaim.aaserial.crc_calculate(data1[1:4], 3)
+        crc = aaserial.crc_calculate(data1[1:4], 3)
         assert crc == 2
 
     def test_feature(self):
+        print('--- feature ---')
         for i in range(0, 5, 1):
             img_url = 'data/test8/img{}.jpg'.format(i)
             print('Load {}'.format(img_url))
@@ -35,17 +37,19 @@ class SerialTestSuite(unittest.TestCase):
                 break
 
     def test_predictor(self):
+        print('--- predictor ---')
         for i in range(0, 5, 1):
             img_url = 'data/test8/img{}.jpg'.format(i)
             print('Load {}'.format(img_url))
             img = helpers.load(img_url)
             predictor = Predictor('weight8.csv')
-            lamps = predictor.predict(img, mode='red', debug=True)
+            lamps = predictor.predict(img, mode='red', debug=False)
             lamps = [x for x in lamps if x.y > 0.5]
             print('   find {} lamp'.format(len(lamps)))
 
     @unittest.skipUnless(port_list and (not isTravis), 'No serial port available.')
     def test_serial_write_and_read(self):
+        print('--- serial ---')
         with serial.Serial('/dev/ttyTHS2', 9600, timeout=1) as ser:
             ser.write(bytearray(data1))
             x = ser.read(100)
