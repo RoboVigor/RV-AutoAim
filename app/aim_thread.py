88d84c91e30dd49aa953257b8cf4fbc4b7f1d563
tccoin <1774241372@qq.com>
2019-06-19 12:19:42 +0800

    多线程优化->90fps

--- app/aim_thread.py
+++ app/aim_thread.py
@@ -1,16 +1,17 @@
 import autoaim
 import cv2
 import time
+import threading
 
+
+# global var
+aim = True
+new_img = None
 ww = 1280
 hh = 720
-camera = autoaim.Camera(1)
-capture = camera.capture
-capture.set(3, ww)
-capture.set(4, hh)
-# suc, img = capture.read()
-# autoaim.helpers.showoff(img)
-# cv2.waitKey(-1)
+
+
+# func
 
 
 def moving_average(last, new):
@@ -20,51 +21,77 @@ def moving_average(last, new):
     return r
 
 
+def load_img():
+    # set up camera
+    global aim, new_img, ww, hh
+    camera = autoaim.Camera(1)
+    capture = camera.capture
+    capture.set(3, ww)
+    capture.set(4, hh)
+    capture.set(cv2.CAP_PROP_AUTO_EXPOSURE, 0.25)
+    capture.set(cv2.CAP_PROP_EXPOSURE, -9)
+    # suc, img = capture.read()
+    # autoaim.helpers.showoff(img)
+    # cv2.waitKey(-1)
+    while aim:
+        suc, new_img = capture.read()
+
+
+# aim
 x_last = [0, 0, 0]
 y_last = [0, 0, 0]
-
+predictor = autoaim.Predictor('weight9.csv')
+img = None
 seq = 0
-fpscount = 0
-last_timestamp = time.time()
-duration = 1
-for i in range(300):
-    suc, img = capture.read()
-    predictor = autoaim.Predictor('weight8.csv')
-    lamps = predictor.predict(
-        img, mode='red', debug=fpscount % 10 == 0, timeout=1)
-    lamps.sort(key=lambda x: x.y)
-    x, y, w, h = (0, 0, 0, 0)
-    lamps = [l for l in lamps if l.y > 0.15]
-    if len(lamps) == 1:
-        x, y, w, h = lamps[-1].bounding_rect
-    elif len(lamps) > 1:
-        x1, y1, w1, h1 = lamps[-1].bounding_rect
-        x2, y2, w2, h2 = lamps[-2].bounding_rect
-        x = (x1+x2)/2
-        y = (y1+y2)/2
-        w = (w1+w2)/2
-        h = (h1+h2)/2
 
-    # current
-    x = (x+w/2)/ww - 0.5
-    y = (y+h/2)/hh - 0.5
+if __name__ == '__main__':
+    threading.Thread(target=load_img).start()
 
-    # avarage
-    x = moving_average(x_last, x)
-    y = moving_average(y_last, y)
+    # fps analysis
+    last_timestamp = time.time()
+    duration = 1
+    fpscount = 0
+    while True:
+        if new_img is None:
+            cv2.waitKey(1)
+            continue
+        img = new_img
+        new_img = None
 
-    # output
-    packet = autoaim.telegram.pack(0x0401, [x*20, -y*6], seq=seq)
-    seq = (seq+1) % 256
-    # autoaim.telegram.send(packet)
-    # cv2.waitKey(10)
+        # autoaim
+        lamps = predictor.predict(
+            img, mode='red', debug=fpscount % 20 == 0, timeout=1)
+        lamps.sort(key=lambda x: x.y)
+        x, y, w, h = (0, 0, 0, 0)
+        lamps = [l for l in lamps if l.y > 0.15]
+        if len(lamps) == 1:
+            x, y, w, h = lamps[-1].bounding_rect
+        elif len(lamps) > 1:
+            x1, y1, w1, h1 = lamps[-1].bounding_rect
+            x2, y2, w2, h2 = lamps[-2].bounding_rect
+            x = (x1+x2)/2
+            y = (y1+y2)/2
+            w = (w1+w2)/2
+            h = (h1+h2)/2
 
-    # calc fps
-    fpscount = seq % 50 + 1
-    if fpscount == 50:
-        duration = time.time() - last_timestamp
-        last_timestamp = time.time()
-        print("fps: ", 50/duration)
+        # current
+        x = (x+w/2)/ww - 0.5
+        y = (y+h/2)/hh - 0.5
 
-    # print('---------')
-    #print(x*30, y)
+        # avarage
+        x = moving_average(x_last, x)
+        y = moving_average(y_last, y)
+
+        # output
+        packet = autoaim.telegram.pack(0x0401, [x*20, -y*6], seq=seq)
+        seq = (seq+1) % 256
+        # autoaim.telegram.send(packet)
+        # cv2.waitKey(10)
+
+        # calc fps
+        fpscount = fpscount % 50 + 1
+        if fpscount == 50:
+            duration = time.time() - last_timestamp
+            last_timestamp = time.time()
+            print("fps: ", 50/duration)
+    print("all over")
---
+++ app/test_camera.py
@@ -0,0 +1,82 @@
+import autoaim
+import cv2
+import time
+import threading
+
+
+# global var
+aim = True
+new_img = None
+ww = 1280
+hh = 720
+fpscount = 0
+
+
+# func
+
+
+def moving_average(last, new):
+    r = sum(last)/len(last)*0.3+new*0.7
+    del last[0]
+    last += [new]
+    return r
+
+
+def load_img():
+    # set up camera
+    global aim, new_img, ww, hh, fpscount
+    camera = autoaim.Camera(1)
+    capture = camera.capture
+    capture.set(3, ww)
+    capture.set(4, hh)
+    capture.set(cv2.CAP_PROP_FPS, 120)
+    print(capture.get(cv2.CAP_PROP_FPS))
+    capture.set(cv2.CAP_PROP_AUTO_EXPOSURE, 0.25)
+    capture.set(cv2.CAP_PROP_EXPOSURE, -9)
+    # suc, img = capture.read()
+    # autoaim.helpers.showoff(img)
+    # cv2.waitKey(-1)
+    while aim:
+        suc, new_img = capture.read()
+        fpscount += 1
+
+
+def aim_enemy():
+    # aim
+    global aim, new_img, ww, hh, fpscount
+    x_last = [0, 0, 0]
+    y_last = [0, 0, 0]
+    predictor = autoaim.Predictor('weight8.csv')
+    img = None
+    # fps analysis
+    seq = 0
+    last_timestamp = time.time()
+    duration = 1
+    while aim:
+        threadLock.acquire()
+        if not new_img is None:
+            # img = new_img.copy()
+            new_img = None
+            threadLock.release()
+        else:
+            threadLock.release()
+            continue
+        fpscount = fpscount % 50 + 1
+        if fpscount == 50:
+            duration = time.time() - last_timestamp
+            last_timestamp = time.time()
+            print("fps: ", 50/duration)
+
+
+# thread
+threadLock = threading.Lock()
+threads = []
+t1 = threading.Thread(target=load_img)
+threads.append(t1)
+t2 = threading.Thread(target=aim_enemy)
+threads.append(t2)
+
+if __name__ == '__main__':
+    for t in threads:
+        t.start()
+    print("all over")
