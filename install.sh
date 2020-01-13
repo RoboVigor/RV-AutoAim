6581d51ccd0a7d47741aa66851787fbb1de65c26
tccoin <1774241372@qq.com>
2020-01-14 00:54:48 +0800

    update version to 3.1

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 cd data
 rm test?/ -rf
-wget -O "data.zip" https://github.com/RoboVigor/RV-AutoAim-Data/archive/3.0.zip
+wget -O "data.zip" https://github.com/RoboVigor/RV-AutoAim-Data/archive/3.1.zip
 unzip data.zip
 mv RV-AutoAim-Data-**/data/* .
 rm RV-AutoAim-Data-* -rf
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('LICENSE') as f:
 
 setup(
     name='AutoAim',
-    version='3.0.0',
+    version='3.1.0',
     description='A project for detecting armors.',
     author='FuXing PS',
     author_email='robovigor@gmail.com',
